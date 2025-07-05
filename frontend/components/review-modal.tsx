8d7e7d3ead499a345add8d1dc13b271dd9afcbca
Pragati Verma <verma.pragati3104@gmail.com>
2025-07-05 12:04:21 +0530

    unused usereact

--- frontend/components/review-modal.tsx
+++ frontend/components/review-modal.tsx
@@ -2,7 +2,7 @@
 
 import type React from "react"
 
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
